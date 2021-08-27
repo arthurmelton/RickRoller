c41b2ccdb3f8869a1b82ec63df5455ac06b92f04
Arthur Melton <amtitan@icloud.com>
2021-08-27 13:34:53 -0400

    init

---
+++ Terminal/src/main.rs
@@ -0,0 +1,13 @@
+use colored::*;
+
+fn main() {
+    let red = "██".red();
+    let black = "██".black();
+    let green = "██".green();
+    let yellow = "██".yellow();
+    let blue = "██".blue();
+    let magenta = "██".magenta();
+    let cyan = "██".cyan();
+    let white = "██".red();
+    
+}
